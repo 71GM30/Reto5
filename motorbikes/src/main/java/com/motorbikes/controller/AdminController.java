be8a27083581e05ea6461383b309e16d1a4667df
71GM30 <wilmar.vega.mt@correo.usa.edu.co>
2021-10-31 08:50:14 -0500

    Yo no queria me obligaron 4

--- motorbikes/src/main/java/com/motorbikes/controller/AdminController.java
+++ motorbikes/src/main/java/com/motorbikes/controller/AdminController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Admin")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class AdminController {
     @Autowired
     private AdminService adminService;
